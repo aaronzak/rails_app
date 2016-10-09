ed118e30d5b595b2cccee9a07f2668ba8ae72d7c
aaronzak <aaronzakhor@gmail.com>
2016-10-09 16:37:53 -0700

    ssl and puma

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -9,6 +9,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    
+  end
+
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -20,4 +25,5 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
   end
+
 end
